a6bda058b777c529297c0c547c8fd0498e2a049b
Andre Ximenes <andreximenes536@gmail.com>
2026-02-06 10:08:28 -0300

    Raiz da api configurada

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Hono } from "hono";
+import type { AppBindings } from "@/@types/declarations";
+import { registerRoutes } from "@/modules";
+import { PrismaDatabase } from "./database/database";
+import { log, showRoutes } from "./lib/dev";
+import environment from "./lib/environment";
+import storage from "./lib/storage";
+import cors from "./middlewares/cors";
+import database from "./middlewares/database";
+import errors from "./middlewares/errors";
+import requestLogger from "./middlewares/logger";
+
+const server = new Hono<AppBindings>({
+	strict: false,
+});
+
+if (environment.ENV === "DEV") {
+	server.use(requestLogger);
+}
+
+server.use(cors);
+server.use(database);
+server.onError(errors);
+registerRoutes(server);
+
+server.notFound((c) => c.json({ message: "Rota não econtrada" }, 404));
+
+if (environment.ENV === "DEV") {
+	showRoutes(server);
+}
+
+log(`Iniciando servidor no modo: ${environment.ENV}`, "info");
+await storage.testConnection();
+await PrismaDatabase.testConnection();
+
+export default server;
