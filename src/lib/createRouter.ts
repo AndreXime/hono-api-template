2cba232065a0cad3d6ef114d829b89eaf31267ae
Andre Ximenes <andreximenes536@gmail.com>
2026-02-13 10:54:21 -0300

    Função createRouter simplificado

--- src/lib/createRouter.ts
+++ src/lib/createRouter.ts
@@ -1,22 +1,9 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import type { AppBindings } from "@/@types/declarations";
-import type { Roles } from "@/database/client/enums";
-import authMiddleware from "@/middlewares/auth";
 import { zodHook } from "@/middlewares/validator";
 
-type CreateRouterOptions = {
-	auth?: boolean;
-	roles?: Roles[];
-};
-
-export default function createRouter(options: CreateRouterOptions = {}) {
-	const app = new OpenAPIHono<AppBindings>({
+export default function createRouter() {
+	return new OpenAPIHono<AppBindings>({
 		defaultHook: zodHook,
 	});
-
-	if (options.auth) {
-		app.use("/*", authMiddleware(options.roles || []));
-	}
-
-	return app;
 }
